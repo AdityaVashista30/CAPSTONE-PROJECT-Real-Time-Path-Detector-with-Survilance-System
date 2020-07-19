cf16e15b12d6452172ff287efa11c55358dde4bf
Aditya Vashista <53120116+AdityaVashista30@users.noreply.github.com>
2020-07-19 10:24:17 +0530

    Update player.py

--- player.py
+++ player.py
@@ -1,133 +1,49 @@
 import sys
-import os.path
-from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtGui import QPalette, QColor
-from PyQt5.QtWidgets import QMainWindow, QWidget, QFrame, QSlider, QHBoxLayout, QPushButton, \
-    QVBoxLayout, QAction, QFileDialog, QApplication
-import vlc
+from PyQt5 import QtCore
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QDialog, QApplication,QMainWindow
+from PyQt5.uic import loadUi
+import sqlite3
 
-class Player(QMainWindow):
 
-    def __init__(self, f=" ",master=None):
-        QMainWindow.__init__(self, master)
-        self.setWindowTitle("Media Player")
-        self.filename=f
-        self.instance = vlc.Instance()
-        self.mediaplayer = self.instance.media_player_new()
-        self.createUI()
-        self.isPaused = False
+class Login(QMainWindow):
+    def __init__(self):
+        self.conn = sqlite3.connect('capstone.db')
+        self.cur = self.conn.cursor()
+        super(Login,self).__init__()
+        loadUi('login.ui',self)
+        self.pushButton.clicked.connect(self.checkLogin)
+        style = open('themes/qdark.css' , 'r')
+        style = style.read()
+        self.setStyleSheet(style)
 
-    def createUI(self):
-        self.widget = QWidget(self)
-        self.setCentralWidget(self.widget)
-        if sys.platform == "darwin": # for MacOS
-            from PyQt5.QtWidgets import QMacCocoaViewContainer	
-            self.videoframe = QMacCocoaViewContainer(0)
+
+    
+    def checkLogin(self):
+        user=self.user.text()
+        password=self.password.text()
+        self.cur.execute('SELECT Password,Type FROM Users WHERE User = ? ', (user,))
+        row = self.cur.fetchone()
+        if row is None:
+            self.label.setText("INVALID USER/Password")
+        elif str(row[0])!=password:
+             self.label.setText("INVALID USER/Password")
         else:
-            self.videoframe = QFrame()
-        self.palette = self.videoframe.palette()
-        self.palette.setColor (QPalette.Window,
-                               QColor(0,0,0))
-        self.videoframe.setPalette(self.palette)
-        self.videoframe.setAutoFillBackground(True)
+            if row[1]=="Admin":
+                self.label.setText("Admin Login Succesful") #TEMP
+            else:
+                self.label.setText("User Login Succesful") #TEMP
 
-        self.positionslider = QSlider(Qt.Horizontal, self)
-        self.positionslider.setToolTip("Position")
-        self.positionslider.setMaximum(1000)
-        self.positionslider.sliderMoved.connect(self.setPosition)
-
-        self.hbuttonbox = QHBoxLayout()
-        self.playbutton = QPushButton("Play")
-        self.hbuttonbox.addWidget(self.playbutton)
-        self.playbutton.clicked.connect(self.PlayPause)
-
-        self.stopbutton = QPushButton("Stop")
-        self.hbuttonbox.addWidget(self.stopbutton)
-        self.stopbutton.clicked.connect(self.Stop)
-
-        self.hbuttonbox.addStretch(1)
-        self.volumeslider = QSlider(Qt.Horizontal, self)
-        self.volumeslider.setMaximum(100)
-        self.volumeslider.setValue(self.mediaplayer.audio_get_volume())
-        self.volumeslider.setToolTip("Volume")
-        self.hbuttonbox.addWidget(self.volumeslider)
-        self.volumeslider.valueChanged.connect(self.setVolume)
-
-        self.vboxlayout = QVBoxLayout()
-        self.vboxlayout.addWidget(self.videoframe)
-        self.vboxlayout.addWidget(self.positionslider)
-        self.vboxlayout.addLayout(self.hbuttonbox)
-
-        self.widget.setLayout(self.vboxlayout)
-
-        open = QAction("&Open", self)
-        open.triggered.connect(self.OpenFile)
-        exit = QAction("&Exit", self)
-        exit.triggered.connect(sys.exit)
-        menubar = self.menuBar()
-        filemenu = menubar.addMenu("&File")
-        filemenu.addAction(open)
-        filemenu.addSeparator()
-        filemenu.addAction(exit)
-
-        self.timer = QTimer(self)
-        self.timer.setInterval(200)
-        self.timer.timeout.connect(self.updateUI)
-
-    def PlayPause(self):
-        if self.mediaplayer.is_playing():
-            self.mediaplayer.pause()
-            self.playbutton.setText("Play")
-            self.isPaused = True
-        else:
-            if self.mediaplayer.play() == -1:
-                self.OpenFile()
-                return
-            self.mediaplayer.play()
-            self.playbutton.setText("Pause")
-            self.timer.start()
-            self.isPaused = False
-
-    def Stop(self):
-
-        self.mediaplayer.stop()
-        self.playbutton.setText("Play")
-
-    def OpenFile(self, filename=None):
-        self.media = self.instance.media_new(self.filename)
-        self.mediaplayer.set_media(self.media)
-        self.media.parse()
-        self.setWindowTitle(self.media.get_meta(0))
-
-        if sys.platform.startswith('linux'): # for Linux using the X Server
-            self.mediaplayer.set_xwindow(self.videoframe.winId())
-        elif sys.platform == "win32": # for Windows
-            self.mediaplayer.set_hwnd(self.videoframe.winId())
-        elif sys.platform == "darwin": # for MacOS
-            self.mediaplayer.set_nsobject(int(self.videoframe.winId()))
-        self.PlayPause()
-
-    def setVolume(self, Volume):
-        self.mediaplayer.audio_set_volume(Volume)
-
-    def setPosition(self, position):
-        self.mediaplayer.set_position(position / 1000.0)
-
-
-    def updateUI(self):
-        self.positionslider.setValue(self.mediaplayer.get_position() * 1000)
-        if not self.mediaplayer.is_playing():
-            self.timer.stop()
-            if not self.isPaused:
-                self.Stop()
-
-if __name__ == "__main__":
+        
+        
+        
+def main():        
     app = QApplication(sys.argv)
-    player = Player()
-    player.show()
-    player.resize(640, 480)
-    if sys.argv[1:]:
-        player.OpenFile(sys.argv[1])
-    sys.exit(app.exec_())
-
-
+    window=Login()
+    window.setWindowTitle('Login')
+    window.show()
+    app.exec_()
+    
+if __name__ == '__main__':
+    main()
