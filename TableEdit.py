bcd25e4ffc85a973879caec967a0d3d392a91b71
Aditya Vashista <53120116+AdityaVashista30@users.noreply.github.com>
2020-07-19 16:52:12 +0530

    Update TableEdit.py

--- TableEdit.py
+++ TableEdit.py
@@ -3,22 +3,25 @@ from datetime import datetime
 
 
 def AddVideo(cam,loc):
-    conn = sqlite3.connect('capstoneDB.db')
+    conn = sqlite3.connect('capstoneSQLDB.db')
     cur = conn.cursor()
-    cur.execute('SELECT code FROM LCODE')
-    code=cur.fetchone()
-    cur.execute('UPDATE LCODE SET code = code + 1 WHERE code = ?',(code[0],))
-    conn.commit()
-    t=datetime.now()
-    date=str(t.date())
-    time=str(t.time())
-    cur.execute('''INSERT INTO VidHistory (Code, Date, Time,Cam,Loc )
-                VALUES (?,?,?,?,?)''', (code[0],date,time,cam,loc))
-    conn.commit()
+    cur.execute('SELECT Loc FROM VidHistory where loc=?',(loc,))
+    temp=cur.fetchone()
+    if temp is None:
+        cur.execute('SELECT code FROM LCODE')
+        code=cur.fetchone()
+        cur.execute('UPDATE LCODE SET code = code + 1 WHERE code = ?',(code[0],))
+        conn.commit()
+        t=datetime.now()
+        date=str(t.date())
+        time=str(t.time())
+        cur.execute('''INSERT INTO VidHistory (Code, Date, Time,Cam,Loc )
+                    VALUES (?,?,?,?,?)''', (code[0],date,time,cam,loc))
+        conn.commit()
     cur.close()
     
 def DelVideo(code):
-    conn = sqlite3.connect('capstoneDB.db')
+    conn = sqlite3.connect('capstoneSQLDB.db')
     cur = conn.cursor()
     cur.execute('Delete FROM VidHistory where code= ?',(code,))
     conn.commit()
