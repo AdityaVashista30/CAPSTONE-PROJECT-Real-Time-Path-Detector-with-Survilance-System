a9244eb52ae8c11ee4d52ab0464a6c0d8499ee89
Aditya Vashista <53120116+AdityaVashista30@users.noreply.github.com>
2020-07-19 11:15:18 +0530

    Update nUser.py

--- nUser.py
+++ nUser.py
@@ -10,9 +10,29 @@ class Login(QMainWindow):
     def __init__(self):
         super(Login,self).__init__()
         loadUi('nUser.ui',self)
+        self.tabWidget.tabBar().setVisible(False)
+        self.handleButtons()
         style = open('themes/qdark.css' , 'r')
         style = style.read()
         self.setStyleSheet(style)
+    
+    def handleButtons(self):
+        self.homePage.clicked.connect(self.openHome)
+        self.liveFPage.clicked.connect(self.openLFoot)
+        self.oldFPage.clicked.connect(self.openOFoot)
+        self.pathPage.clicked.connect(self.openTracker)
+        self.logoutPage.clicked.connect(self.openLogout)
+        
+    def openHome(self):
+        self.tabWidget.setCurrentIndex(0)
+    def openLFoot(self):
+        self.tabWidget.setCurrentIndex(1)
+    def openOFoot(self):
+        self.tabWidget.setCurrentIndex(2)
+    def openTracker(self):
+        self.tabWidget.setCurrentIndex(3)
+    def openLogout(self):
+        self.tabWidget.setCurrentIndex(4)
 
 
 
