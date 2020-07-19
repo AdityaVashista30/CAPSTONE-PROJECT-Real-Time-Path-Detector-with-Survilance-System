3b9ca43186302d907effe02a277e09bd58b9ae26
Aditya Vashista <53120116+AdityaVashista30@users.noreply.github.com>
2020-07-19 11:03:02 +0530

    Add files via upload

---
+++ nUser.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt5 import QtCore
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QDialog, QApplication,QMainWindow
+from PyQt5.uic import loadUi
+import sqlite3
+
+class Login(QMainWindow):
+    def __init__(self):
+        super(Login,self).__init__()
+        loadUi('nUser.ui',self)
+
+
+
+
+def main():        
+    app = QApplication(sys.argv)
+    window=Login()
+    window.setWindowTitle('Login')
+    window.show()
+    app.exec_()
+    
+if __name__ == '__main__':
+    main()
