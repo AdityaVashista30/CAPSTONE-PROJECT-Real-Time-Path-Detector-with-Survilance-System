d48ebf2163970f8ac78fadd3faefea643768e1cd
Aditya Vashista <53120116+AdityaVashista30@users.noreply.github.com>
2020-07-19 09:38:49 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5 import QtCore
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.uic import loadUi
+from main2 import Life2Coding
+import sqlite3
+
+class Login(QDialog):
+    
+s
